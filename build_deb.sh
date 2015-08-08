d5d0f7a0935ffd0fd27182f66219945b7c4a4d75
msfabxda <fabian.s90s@googlemail.com>
2015-08-08 20:18:02 +0200

    Delete build_deb.sh

--- build_deb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MAINTAINER="mauro.ribeiro@hardkernel.com"
-
-PKGVER=`date +%Y%m%d`
-GITREV=`git rev-parse --short HEAD`
-
-checkinstall --pkgname="cloudshell_lcd" --pkgversion="$PKGVER" -A armhf --maintainer=\"$MAINTAINER\" --pkggroup="other" --pkglicense="GPLv2" --requires="curl,sysstat" --nodoc -y -d2 --pkgrelease="2" -D --install=no
-
